9ce98d0f2802ddea6c9fba445fe99ec92925fbc8
xiyan678 <73523532+xiyan678@users.noreply.github.com>
2025-01-04 12:03:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -51,7 +51,7 @@ export default defineConfig(({ mode }) => {
           ],
         },
         manifest: {
-          name: "今日热榜",
+          name: "优质新闻",
           short_name: "DailyHot",
           description: "汇聚全网热点，热门尽览无余",
           display: "standalone",
